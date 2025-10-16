159c00de2f0da426c95fb564c70a10559d5b7831
Makrem Taieb <makrem16790@gmail.com>
2025-10-16 23:01:54 +0100

    add azure client secret

--- APIs_Signature_DigiGO/Services/LegalisationService.cs
+++ APIs_Signature_DigiGO/Services/LegalisationService.cs
@@ -235,7 +235,6 @@ namespace APIs_Signature_DigiGO.Services
                         Department = "IT",
                         Type = "Client"
                     });
-
                 }
                 catch (KeyNotFoundException knf)
                 {
